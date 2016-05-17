ac93cd03eaac8de9d0499553198a12fd5cb97039
Dennis Starke <dennis.starke@fh-bielefeld.de>
2016-05-17 19:47:31 +0200

    componenten hinzuegefuegt /Views

---
+++ components/ViewContainer.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Dennis on 17.05.2016.
+ */
+'use strict'
+import React, { Component, View } from 'react-native'
+
+class ViewContainer extends Component {
+    render() {
+        return (
+            <View style={styles.viewContainer}>
+                {this.props.children}
+            </View>
+        )
+    }
+}
+
+const styles = React.StyleSheet.create({
+
+    viewContainer: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "stretch"
+    }
+
+})
+
+module.exports = ViewContainer
