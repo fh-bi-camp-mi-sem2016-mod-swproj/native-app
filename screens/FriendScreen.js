422987e8dc0fa65541d242327062d75a18028b87
Dennis Starke <dennis.starke@fh-bielefeld.de>
2016-06-15 15:16:17 +0200

    - LIstView mit statischen Daten

--- screens/FriendScreen.js
+++ screens/FriendScreen.js
@@ -1,18 +1,46 @@
 /**
  * Created by Dennis on 18.05.2016.
  */
-import React, {AppRegistry, Component, StyleSheet, Text, View, TouchableHighlight, button, TextInput, Image, Alert,} from 'react-native';
+import React, {AppRegistry, Component, StyleSheet, Text, View, TouchableHighlight, button, TextInput, Image, Alert, ListView} from 'react-native';
 import ViewContainer from  '../components/frontend/ViewContainer'
-import StatusBarBackground from  '../components/frontend/StatusBarBackground'
 import ButtonContainer from '../components/frontend/ButtonContainer'
 
+var data;
 
 class FriendScreen extends Component {
 
+    constructor(props){
+        super(props);
+
+        data =[{User: "Ruben", Info: "beleidigung"},
+            {User: "Bene", Info: "belästigung"},
+            {User: "Dennis", Info: "geilheit"},
+            {User: "Florian", Info: "dauerdruck"},
+            {User: "Fynn", Info: "Wasser"}];
+
+        var ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 != r2
+        });
+
+        this.state = {
+            dataSource: ds.cloneWithRows(data)
+        }
+    }
+
+    renderRow(rowData) {
+        return (
+            <View style={styles.inputListView}>
+                <Text style={{fontSize:18}}>
+                    {rowData.User +" "+ rowData.Info}
+                </Text>
+            </View>
+        )
+    }
 
     render() {
         return (
             <ViewContainer>
+
                 <View style={styles.titleView}>
                     <Text style={styles.titleText}>
                         Welcome to Find.me
@@ -28,16 +56,27 @@ class FriendScreen extends Component {
                 </ButtonContainer>
                 <ViewContainer>
 
+                    <ListView
+                        dataSource={this.state.dataSource}
+                        renderRow={(rowData) => {return this.renderRow(rowData)}}>
+                    </ListView>
 
                 </ViewContainer>
+
+
+                <ButtonContainer>
+                    <TouchableHighlight onPress={(event) => this._add()}>
+                        <Text style={styles.btnText}> add </Text>
+                    </TouchableHighlight>
+                </ButtonContainer>
+
                 <ButtonContainer>
                     <TouchableHighlight onPress={(event) => this._navigateToMainMenue()}>
                         <Text style={styles.btnText}> Back </Text>
                     </TouchableHighlight>
                 </ButtonContainer>
-                <StatusBarBackground />
-            </ViewContainer>
 
+            </ViewContainer>
 
         );
     }
@@ -52,7 +91,18 @@ class FriendScreen extends Component {
         this.props.navigator.push({
             ident: "UserSearch"
         })
+    }
+    _add(){
+        var ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 != r2
+        });
 
+        //data.push({User: "Test", Info: "Bla"});
+        data = data.concat([{User: "Test2", Info: "Blup"}]);
+        this.setState({dataSource: ds.cloneWithRows(data) });
+        //concat bei Liste
+        Alert.alert('Test', data[5].User+" "+data[5].Info, [{text: 'ok'}]);
+        //Alert.alert('Test', data[6].User+" "+data[6].Info, [{text: 'ok'}]);
     }
 }
 
@@ -90,6 +140,13 @@ const styles = StyleSheet.create({
         padding: 5,
         height: 20,
         margin: 10
+    },
+    inputListView: {
+        padding: 5,
+        marginRight: 50,
+        marginLeft: 50,
+        flex: 1,
+        alignSelf: "flex-start"
     }
 });
 
