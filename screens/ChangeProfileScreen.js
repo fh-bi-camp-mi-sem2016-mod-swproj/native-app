49d3245f9e49faa58adb4abbdc0b6431208d6afd
dbene <bene.strz@googlemail.com>
2016-07-15 10:42:34 +0200

    friendslist gets created

--- screens/ChangeProfileScreen.js
+++ screens/ChangeProfileScreen.js
@@ -277,15 +277,38 @@ class ChangeProfileScreen extends Component {
 
                 };
 
-                var newFriendlist = {
-                    "doctype": "friends",
-                    "profile_id": User.getInstance().currentUSER.user._id,
-                    "friends": []
-                };
-
                 if(data.length == 0) {
-                    db.profile.create(newProfile, callbacks);
-                    db.friends.create(newFriendlist, callbacks);
+                    var callbacksProfilCreate = {
+                        success: function (data) {
+                            console.log(data);
+                            Alert.alert('Erfolg', "Profil erfolgreich erstellt.", [{text: 'ok'}]);
+                            User.getInstance().currentUSER.profile = newProfile;
+
+                            var newFriendlist = {
+                                "doctype": "friends",
+                                "profile_id": data.id,
+                                "friends": []
+                            };
+
+                            var callbacksFriend = {
+                                success: function (data) {
+                                    console.log(data);
+                                    Alert.alert('Erfolg', JSON.stringify(newFriendlist), [{text: 'ok'}]);
+                                },
+                                error: function (error) {
+                                    console.log(error);
+                                    Alert.alert('Fehler', "Es gab einen Fehler bei der Datenbankanfrage.", [{text: 'ok'}]);
+                                }
+                            };
+
+                            db.friends.create(newFriendlist, callbacksFriend);
+                        },
+                        error: function (error) {
+                            console.log(error);
+                            Alert.alert('Fehler', "Es gab einen Fehler bei der Datenbankanfrage.", [{text: 'ok'}]);
+                        }
+                    };
+                    db.profile.create(newProfile, callbacksProfilCreate);
                 }
                 else if (data.length == 1) {
                     if(User.getInstance().currentUSER.profile._id) {
@@ -325,16 +348,6 @@ class ChangeProfileScreen extends Component {
                     }
 
                 }
-            },
-            error: function (error) {
-                console.log(error);
-                Alert.alert('Fehler', "Es gab einen Fehler bei der Datenbankanfrage.", [{text: 'ok'}]);
-            }
-        };
-        var callbacks = {
-            success: function (data) {
-                console.log(data);
-                Alert.alert('Erfolg', "Profil erfolgreich erstellt.", [{text: 'ok'}]);
             },
             error: function (error) {
                 console.log(error);
